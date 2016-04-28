dc903619cf68684dccf0c3323fb842917a4b4f23
Derek <der.tse@gmail.com>
2016-04-28 11:00:45 +0200

    added app.config

--- www/app.js
+++ www/app.js
@@ -1,10 +1,19 @@
-console.log("Hello world!");
-
 // declare our angular app
 // and it's dependencies
-angular.module("myApp", [
+var app = angular.module("myApp", [
   'ngRoute',
   'ngResource',
   'ngTouch',
   'ui.bootstrap'
 ]);
+
+app.config(["$locationProvider", "$routeProvider", function ($locationProvider, $routeProvider){
+
+	$routeProvider
+	.when("/", {
+		templateUrl: "/template/home.html"
+	})
+	.otherwise({
+		templateUrl: "/template/404.html"
+	})
+}]);
