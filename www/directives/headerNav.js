cd6603a3ddd28268f66a10b83c28d7d425ef1b60
Derek <der.tse@gmail.com>
2016-04-29 15:26:52 +0200

    updated navbar with aleks

--- www/directives/headerNav.js
+++ www/directives/headerNav.js
@@ -2,7 +2,34 @@ app.directive('headerNav', [function(){
 
   return {
     templateUrl: '/directives/headerNav.html',
-    controller: ['$scope', '$interval', function($scope, $interval) {
+    controller: ['$scope', '$interval', '$location', function($scope, $interval, $location) {
+
+      $scope.links = [
+        {
+          title: "HEM",
+          url: "/"
+        },
+        {
+          title: "KÖPA",
+          url: "/buy"
+        },
+        {
+          title: "SÄLJA",
+          url: "/sell"
+        },
+        {
+          title: "OM",
+          url: "/about"
+        },
+        {
+          title: "KONTAKT",
+          url: "/contact"
+        },
+      ];
+
+      $scope.currentUrl = function() {
+        return $location.url();
+      }
       
       function currentTime() {
         $scope.currentTime = new Date();
