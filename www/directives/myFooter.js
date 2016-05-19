157f7ab4906b801c0d85bc7a3449ff62eb78518e
hamzeh Mahmoud <hamzeh Mahmoud>
2016-05-19 19:41:43 +0200

    ny footer, senaste bostäder

---
+++ www/directives/myFooter.js
@@ -0,0 +1,13 @@
+// directive that loads ui bootstraps carousel
+app.directive('myFooter', [function () {
+
+  return {
+    templateUrl: '/directives/myFooter.html',
+    // template: '<div>Hej!</div>'
+    controller: ['$scope', function($scope) {
+
+   
+
+    }]
+  };
+}]);
