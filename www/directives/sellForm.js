9bb5c73a35fdb83798aac40061250fe2651bced7
Derek <der.tse@gmail.com>
2016-05-20 14:20:31 +0200

    added sell directive js

--- www/directives/sellForm.js
+++ www/directives/sellForm.js
@@ -2,6 +2,14 @@ app.directive('sellForm', [function() {
 
 	return {
 		templateUrl: '/directives/sellForm.html',
-		controller: 
+		controller: ['$scope', 'Property', function ($scope, Property) {
+
+			$scope.submitForm = function (){
+				//create new Property when form is submitted
+				Property.create($scope.newProp);
+				console.log($scope.newProp, "created")
+				$scope.newProp = null;
+			}
+		}]
 	}
 }]);
