615421e5ee2066110ab3dad65bac6c5385c8135d
hamzeh Mahmoud <hamzeh Mahmoud>
2016-05-20 12:10:40 +0200

    Fixed carousel

--- www/directives/mainContent.js
+++ www/directives/mainContent.js
@@ -5,27 +5,6 @@ app.directive('mainContent', [function(){
     controller: ['$scope', function($scope) {
 
       $scope.wells = $scope.myInterval = 5000;
-      $scope.noWrapSlides = false;
-      $scope.active = 0;
-      var slides = $scope.slides = [];
-      var currIndex = 0;
-
-      $scope.addSlide = function() {
-        var newWidth = 600 + slides.length + 1;
-        slides.push({
-          //image: 'http://lorempixel.com/' + newWidth + '/300',
-          imageStyle: {
-            'background-image': 'url(http://lorempixel.com/' + newWidth + '/300)'
-          },
-          text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
-          id: currIndex++
-        });
-      };
-    
-      for (var i = 0; i < 4; i++) {
-        $scope.addSlide();
-      }
-
 
       $scope.colSize = Math.floor(24/$scope.wells.length);
     }]
--- www/directives/myCarousel.js
+++ www/directives/myCarousel.js
@@ -3,26 +3,30 @@ app.directive('myCarousel', [function () {
 
   return {
     templateUrl: '/directives/myCarousel.html',
-    // template: '<div>Hej!</div>'
+    // template: '<div>Hej!</div>',
     controller: ['$scope', function($scope) {
-      $scope.myInterval = 5000;
+      $scope.myInterval = 3000;
       $scope.noWrapSlides = false;
       $scope.active = 0;
       var slides = $scope.slides = [];
       var currIndex = 0;
-
       $scope.addSlide = function() {
-        var newWidth = 600 + slides.length + 1;
-        slides.push({
-          image: 'http://lorempixel.com/' + newWidth + '/300',
+        
+        slides.push({ 
+          imageStyle: {
+            'background-image': 'url(/img/vila%20' + (currIndex+1) + '/bild' + (currIndex+1) + '-1.jpg)'
+          },
           text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
-          id: currIndex++
+          id: currIndex
         });
+        currIndex++;
       };
 
-      for (var i = 0; i < 4; i++) {
+      for (var i = 0; i < 7; i++) {
         $scope.addSlide();
       }
+
+      console.log("slides", slides)
     }]
   };
 }]);
