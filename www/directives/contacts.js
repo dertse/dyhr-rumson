15f9449c92d5c1e594198866df1dea6a7e36feac
dev0l <marcus.mobark@gmail.com>
2016-05-21 20:34:26 +0200

    changes: design/form

--- www/directives/contacts.js
+++ www/directives/contacts.js
@@ -1,9 +1,16 @@
-app.directive('contacts', [function() {
-	
+app.directive('contacts', [function () {
+
 	return {
 		templateUrl: '/directives/contacts.html',
-		controller: ['$scope','$location','$resource','$routeParams', function($scope,$location,$resource,$routeParams){
-				
+		controller: ['$scope', '$location', '$resource', '$routeParams', function ($scope, $location, $resource, $routeParams) {
+
+			$scope.submitCont = function () {
+				//create new Contact when form is submitted
+				Contact.create($scope.newCont);
+				console.log($scope.newCont, "created")
+				$scope.newCont = null;
+			}
+
 			// Regions			
 			$scope.regions = [{
 
@@ -11,25 +18,25 @@ app.directive('contacts', [function() {
 				location: "Stockholm",
 				adress: "Exempelgatan 0, Exempel",
 				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id gravida ligula. Nulla vel vestibulum nulla. Maecenas aliquam eros ac felis iaculis, ac blandit risus hendrerit. Mauris sed scelerisque mi. Ut magna mi, cursus ut porttitor a, condimentum blandit ipsum. Nulla aliquet iaculis nunc non vehicula. Sed id molestie sem, a tempor ipsum. Sed feugiat turpis in elit hendrerit gravida.",
-				pic:"img/region/stockholm.png"
+				pic: "img/region/stockholm.png"
 			},
-			{
-				name: "Region 2",
-				location: "Göteborg",
-				adress: "Exempelgatan 0, Exempel",
-				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id gravida ligula. Nulla vel vestibulum nulla. Maecenas aliquam eros ac felis iaculis, ac blandit risus hendrerit. Mauris sed scelerisque mi. Ut magna mi, cursus ut porttitor a, condimentum blandit ipsum. Nulla aliquet iaculis nunc non vehicula. Sed id molestie sem, a tempor ipsum. Sed feugiat turpis in elit hendrerit gravida.",
-				pic:"img/region/goteborg.png"
+				{
+					name: "Region 2",
+					location: "Göteborg",
+					adress: "Exempelgatan 0, Exempel",
+					description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id gravida ligula. Nulla vel vestibulum nulla. Maecenas aliquam eros ac felis iaculis, ac blandit risus hendrerit. Mauris sed scelerisque mi. Ut magna mi, cursus ut porttitor a, condimentum blandit ipsum. Nulla aliquet iaculis nunc non vehicula. Sed id molestie sem, a tempor ipsum. Sed feugiat turpis in elit hendrerit gravida.",
+					pic: "img/region/goteborg.png"
 
-			},
-			{
-				name: "Region 3",
-				location: "Malmö",
-				adress: "Exempelgatan 0, Exempel",
-				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id gravida ligula. Nulla vel vestibulum nulla. Maecenas aliquam eros ac felis iaculis, ac blandit risus hendrerit. Mauris sed scelerisque mi. Ut magna mi, cursus ut porttitor a, condimentum blandit ipsum. Nulla aliquet iaculis nunc non vehicula. Sed id molestie sem, a tempor ipsum. Sed feugiat turpis in elit hendrerit gravida.",
-				pic:"img/region/malmo.png"
+				},
+				{
+					name: "Region 3",
+					location: "Malmö",
+					adress: "Exempelgatan 0, Exempel",
+					description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id gravida ligula. Nulla vel vestibulum nulla. Maecenas aliquam eros ac felis iaculis, ac blandit risus hendrerit. Mauris sed scelerisque mi. Ut magna mi, cursus ut porttitor a, condimentum blandit ipsum. Nulla aliquet iaculis nunc non vehicula. Sed id molestie sem, a tempor ipsum. Sed feugiat turpis in elit hendrerit gravida.",
+					pic: "img/region/malmo.png"
 
-			}];
-			
+				}];
+
 			// Offices
 			$scope.offices = [{
 
@@ -41,32 +48,32 @@ app.directive('contacts', [function() {
 				room: 9,
 				size: 325,
 				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id gravida ligula. Nulla vel vestibulum nulla. Maecenas aliquam eros ac felis iaculis, ac blandit risus hendrerit. Mauris sed scelerisque mi. Ut magna mi, cursus ut porttitor a, condimentum blandit ipsum. Nulla aliquet iaculis nunc non vehicula. Sed id molestie sem, a tempor ipsum. Sed feugiat turpis in elit hendrerit gravida.",
-				pic:"img/kontor/kontor1.jpg"
+				pic: "img/kontor/kontor1.jpg"
 			},
-			{
-				name: "Göteborg",
-				location: "Göteborg",
-				adress: "Exempelgatan 0, Exempel",
-				email: "blabla@blabla.bla",
-				phone: "0000-00 00 00",
-				room: 8,
-				size: 245,
-				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id gravida ligula. Nulla vel vestibulum nulla. Maecenas aliquam eros ac felis iaculis, ac blandit risus hendrerit. Mauris sed scelerisque mi. Ut magna mi, cursus ut porttitor a, condimentum blandit ipsum. Nulla aliquet iaculis nunc non vehicula. Sed id molestie sem, a tempor ipsum. Sed feugiat turpis in elit hendrerit gravida.",
-				pic:"img/kontor/kontor2.jpg"
+				{
+					name: "Göteborg",
+					location: "Göteborg",
+					adress: "Exempelgatan 0, Exempel",
+					email: "blabla@blabla.bla",
+					phone: "0000-00 00 00",
+					room: 8,
+					size: 245,
+					description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id gravida ligula. Nulla vel vestibulum nulla. Maecenas aliquam eros ac felis iaculis, ac blandit risus hendrerit. Mauris sed scelerisque mi. Ut magna mi, cursus ut porttitor a, condimentum blandit ipsum. Nulla aliquet iaculis nunc non vehicula. Sed id molestie sem, a tempor ipsum. Sed feugiat turpis in elit hendrerit gravida.",
+					pic: "img/kontor/kontor2.jpg"
 
-			},
-			{
-				name: "Malmö",
-				location: "Malmö",
-				adress: "Exempelgatan 0, Exempel",
-				email: "blabla@blabla.bla",
-				phone: "0000-00 00 00",
-				room: 4,
-				size: 138,
-				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id gravida ligula. Nulla vel vestibulum nulla. Maecenas aliquam eros ac felis iaculis, ac blandit risus hendrerit. Mauris sed scelerisque mi. Ut magna mi, cursus ut porttitor a, condimentum blandit ipsum. Nulla aliquet iaculis nunc non vehicula. Sed id molestie sem, a tempor ipsum. Sed feugiat turpis in elit hendrerit gravida.",
-				pic:"img/kontor/kontor3.jpg"
-			}];
-			
+				},
+				{
+					name: "Malmö",
+					location: "Malmö",
+					adress: "Exempelgatan 0, Exempel",
+					email: "blabla@blabla.bla",
+					phone: "0000-00 00 00",
+					room: 4,
+					size: 138,
+					description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id gravida ligula. Nulla vel vestibulum nulla. Maecenas aliquam eros ac felis iaculis, ac blandit risus hendrerit. Mauris sed scelerisque mi. Ut magna mi, cursus ut porttitor a, condimentum blandit ipsum. Nulla aliquet iaculis nunc non vehicula. Sed id molestie sem, a tempor ipsum. Sed feugiat turpis in elit hendrerit gravida.",
+					pic: "img/kontor/kontor3.jpg"
+				}];
+
 			// Employees
 			$scope.employees = [{
 
@@ -77,29 +84,29 @@ app.directive('contacts', [function() {
 				phone: "0000-00 00 00",
 				price: 500,
 				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id gravida ligula. Nulla vel vestibulum nulla. Maecenas aliquam eros ac felis iaculis, ac blandit risus hendrerit. Mauris sed scelerisque mi. Ut magna mi, cursus ut porttitor a, condimentum blandit ipsum. Nulla aliquet iaculis nunc non vehicula. Sed id molestie sem, a tempor ipsum. Sed feugiat turpis in elit hendrerit gravida.",
-				pic:"img/bild4.jpg"
+				pic: "img/bild4.jpg"
 			},
-			{
-				name: "sucraM",
-				location: "Göteborg",
-				adress: "Bodilsgatan 4A, Fridhem",
-				email: "blabla@blabla.bla",
-				phone: "0000-00 00 00",
-				price: 500,
-				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id gravida ligula. Nulla vel vestibulum nulla. Maecenas aliquam eros ac felis iaculis, ac blandit risus hendrerit. Mauris sed scelerisque mi. Ut magna mi, cursus ut porttitor a, condimentum blandit ipsum. Nulla aliquet iaculis nunc non vehicula. Sed id molestie sem, a tempor ipsum. Sed feugiat turpis in elit hendrerit gravida.",
-				pic:"img/bild4.jpg"
-			},
-			{
-				name: "skelA",
-				location: "Malmö",
-				adress: "Bodilsgatan 4A, Fridhem",
-				email: "blabla@blabla.bla",
-				phone: "0000-00 00 00",
-				price: 500,
-				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id gravida ligula. Nulla vel vestibulum nulla. Maecenas aliquam eros ac felis iaculis, ac blandit risus hendrerit. Mauris sed scelerisque mi. Ut magna mi, cursus ut porttitor a, condimentum blandit ipsum. Nulla aliquet iaculis nunc non vehicula. Sed id molestie sem, a tempor ipsum. Sed feugiat turpis in elit hendrerit gravida.",
-				pic:"img/bild4.jpg"
-			}];
-			
+				{
+					name: "sucraM",
+					location: "Göteborg",
+					adress: "Bodilsgatan 4A, Fridhem",
+					email: "blabla@blabla.bla",
+					phone: "0000-00 00 00",
+					price: 500,
+					description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id gravida ligula. Nulla vel vestibulum nulla. Maecenas aliquam eros ac felis iaculis, ac blandit risus hendrerit. Mauris sed scelerisque mi. Ut magna mi, cursus ut porttitor a, condimentum blandit ipsum. Nulla aliquet iaculis nunc non vehicula. Sed id molestie sem, a tempor ipsum. Sed feugiat turpis in elit hendrerit gravida.",
+					pic: "img/bild4.jpg"
+				},
+				{
+					name: "skelA",
+					location: "Malmö",
+					adress: "Bodilsgatan 4A, Fridhem",
+					email: "blabla@blabla.bla",
+					phone: "0000-00 00 00",
+					price: 500,
+					description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id gravida ligula. Nulla vel vestibulum nulla. Maecenas aliquam eros ac felis iaculis, ac blandit risus hendrerit. Mauris sed scelerisque mi. Ut magna mi, cursus ut porttitor a, condimentum blandit ipsum. Nulla aliquet iaculis nunc non vehicula. Sed id molestie sem, a tempor ipsum. Sed feugiat turpis in elit hendrerit gravida.",
+					pic: "img/bild4.jpg"
+				}];
+
 		}]
 	};
 }]);
