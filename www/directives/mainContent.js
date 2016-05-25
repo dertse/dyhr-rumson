870e0cbca5013052d054af0f1400f328146ad355
Derek <der.tse@gmail.com>
2016-05-25 22:55:26 +0200

    cleaned up some code and indentation

--- www/directives/mainContent.js
+++ www/directives/mainContent.js
@@ -4,6 +4,9 @@ app.directive('mainContent', [function(){
     templateUrl: '/directives/mainContent.html',
     controller: ['$scope', function($scope) {
 
+      var latest = [];
+      
+
       $scope.wells = $scope.myInterval = 5000;
 
       $scope.colSize = Math.floor(24/$scope.wells.length);
