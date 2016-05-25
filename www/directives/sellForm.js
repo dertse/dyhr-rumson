2a5bba0f1de78ad46a60b5c903778cdfedabd4c4
Derek <der.tse@gmail.com>
2016-05-25 21:27:31 +0200

    update to sellForm.html

--- www/directives/sellForm.js
+++ www/directives/sellForm.js
@@ -4,12 +4,35 @@ app.directive('sellForm', [function() {
 		templateUrl: '/directives/sellForm.html',
 		controller: ['$scope', 'Property', function ($scope, Property) {
 
+			// fill dropdownmenu with img
+			$scope.urls = [];
+			var apartments = [], houses = [];
+
+			(function (){
+				for (var i = 0; i < 8; i++){
+					apartments.push("/img/l/bild" + (i+1) + ".jpg");
+					houses.push("/img/v/bild" + (i+1) + ".jpg");
+				}
+			})();
+
+			$scope.update = function (){
+				if ($scope.newProp.type === "Lägenhet") {
+					$scope.urls = apartments;
+				}
+				else if ($scope.newProp.type === "Villa") {
+					$scope.urls = houses;
+				}
+				console.log($scope.urls)
+			}
+
+
+			//create new Property when form is submitted
 			$scope.submitForm = function (){
-				//create new Property when form is submitted
 				Property.create($scope.newProp);
 				console.log($scope.newProp, "created")
 				$scope.newProp = null;
 			}
+
 		}]
 	}
 }]);
